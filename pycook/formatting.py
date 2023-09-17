4f09a35ae22fa1dcb849b65f320c2f2f8c8c339e
Paul Saary <github@paulsaary.de>
2023-09-17 12:49:00 +0200

    add formatting

---
+++ pycook/formatting.py
@@ -0,0 +1,25 @@
+from typing import Union
+
+from pycook.types import Cookware, Ingredient
+
+
+def item_table(items: list[Union[Ingredient, Cookware]]) -> str:
+    rows = []
+    name_len = max([len(str(item.name)) for item in items])
+    unit_len = max([len(str(item.unit)) for item in items])
+
+    header_line = "-"*(unit_len ) + " |" + "-"*(name_len )
+    
+    rows.append("| How much  | What |")
+    rows.append(f"| {header_line} |")
+
+    for item in items:
+        unit_space = ' '*(unit_len - len(str(item.unit)))
+        name_space = ' '*(name_len - len(str(item.name)))
+        if item.unit is None:
+            row = f"| | {item.name}{name_space} |"
+        else:
+            row = f"| **{str(item.unit)}**{unit_space} | {item.name}{name_space} |"
+        rows.append(row)
+
+    return "\n".join(rows)
